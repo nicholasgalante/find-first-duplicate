1709d4c1b9425d87eaa4d969744838d322911d5a
Nicholas Galante <nicholasgalante94@gmail.com>
2022-12-29 12:47:30 -0500

    Add pseudocode

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -15,5 +15,23 @@ if (require.main === module) {
 
 module.exports = findFirstDuplicate;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/* Pseudocode:
+create a for loop to iterate over each index of array
+  compare the first index to every remaining value
+    if the first index equals any other remaining value, return that index
+    if not, compare second index
+  repeat
+If no duplicates are found, return -1
+*/
+
+
+/*  Written explanation of your solution
+Using Javascript, write a function findFirstDuplicate that receives one argument, an array. This function should iterate over the array and find the first duplicate value that occurs. If a duplicate exists, the function should return that duplicate. If no duplicates exist, the function should return -1. 
+
+Input: [2, 1, 3, 3, 2]
+Output: 3
+
+Input: [1, 2, 3, 4]
+Output: -1
+*/
+
